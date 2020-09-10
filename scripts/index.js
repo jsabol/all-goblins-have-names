162fd59a80cf9d0019e252bd162b1daef64d9963
Juliet Sabol <julietsabol@gmail.com>
2020-09-09 22:09:09 -0400

    Trim name, add error handling

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,10 @@
 function startAGHN() {
   Hooks.on("preCreateToken", (scene, tokenData) => {
-    if (tokenData.name.startsWith("@RollTable[")) {
+    const displayName = tokenData.name.trim();
+    if (displayName.startsWith("@RollTable[")) {
       // get the table by its ID
-      const endIndex = tokenData.name.indexOf("]");
-      const table_id = tokenData.name.substring(11, endIndex);
+      const endIndex = displayName.indexOf("]");
+      const table_id = displayName.substring(11, endIndex);
       const table = game.tables.entities.find((t) => t._id === table_id);
 
       if (table) {
@@ -11,6 +12,10 @@ function startAGHN() {
         // join all the results together with spaces
         const resultsArr = roll.results.map((r) => r.text);
         tokenData.name = resultsArr.join(" ");
+      } else {
+        ui.notifications.error(
+          "Can't find a table that matches " + displayName
+        );
       }
     }
   });
